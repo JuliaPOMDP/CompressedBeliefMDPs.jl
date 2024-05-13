bbe759279f10682c093971dd2a39b2a584584a29
Logan Mondal Bhamidipaty <76822456+FlyingWorkshop@users.noreply.github.com>
2024-05-12 20:01:27 -0700

    qualified uniform

--- src/envs/circular_corridors.jl
+++ src/envs/circular_corridors.jl
@@ -1,12 +1,27 @@
-struct CircularCorridorsState
-    is_top::Bool
+struct CircularCorridorState
+    row::Integer
     index::Integer
 end
 
 
-struct CircularCorridorsPOMDP <: POMDP{CircularCorridorsState, Integer, Integer}
-    top_goal::CircularCorridorsState
-    bot_goal::CircularCorridorsState
+struct CircularCorridorPOMDP <: POMDP{CircularCorridorState, Integer, Integer}
+    corridor_length::Integer
+    num_corridors::Integer
+    distribution::VonMises
+    goals::AbstractArray{CircularCorridorState}
+end
+
+
+function CircularCorridorPOMDP(corridor_length::Integer; num_corridors::Integer=2)
+    goals = []
+    goal_indices = rand(1:corridor_length, num_corridors)
+    for (row, index) in enumerate(goal_indices)
+        goal = CircularCorridorState(row, index)
+        push!(goals, goal)
+    end
+    distribution = VonMises()
+    pomdp = CircularCorridorPOMDP(corridor_length, num_corridors, distribution, goals)
+    return pomdp
 end
 
 
@@ -17,30 +32,42 @@ const SENSE_CORRIDOR = 2
 const DECLARE_GOAL = 3
 
 
-function states(p::CircularCorridorsPOMDP)
+function POMDPs.states(p::CircularCorridorPOMDP)
     return nothing
 end
 
 
-function stateindex(p::CircularCorridorsPOMDP, s::CircularCorridorsState)
+function POMDPs.stateindex(p::CircularCorridorPOMDP, s::CircularCorridorState)
     return nothing
 end
 
 
-# function observation(p::CircularCorridorsPOMDP, a::Integer, sp::CircularCorridorsState)
-#     if a == SENSE_CORRIDOR
-#         if sp.is_top
-#             return sp === p.top_goal
-#         else
-#             return sp === p.bot_goal
-#         end
-#     else
-#         # TODO: von Mises distribution
-#     end
-# end
+function _sample_distribution(p::CircularCorridorsPOMDP)
+    sample = rand(p.distribution)
+    min_ = minimum(p.distribution)
+    max_ = maximum(p.distribution)
+    step = (max_ - min_) / p.corridor_length
+    bins = collect(min_:step:max_)
+    i = searchsortedfirst(bins, sample)  # TODO: replace this w/ NN search? # FIXME: searchsortedfirst doesn't work as needed
+    return i
+end
 
 
-# function transition(p::CircularCorridorsPOMDP)
+function POMDPs.observation(p::CircularCorridorsPOMDP, a::Integer, sp::CircularCorridorsState)
+    if a == SENSE_CORRIDOR
+        return sp.row
+    else
+        μ = sp.index
+        sample = _sample_distribution(p)
+        index = (μ + sample) % p.corridor_length
+        return index
+    end
+end
+
+
+function POMDPs.transition(p::CircularCorridorsPOMDP)
+    return nothing
+end
 
 
 
